7ee2bba9e253924fe077c6a3501746ea89a00c52
Leonard22-del <leonardmolua20@gmail.com>
2024-12-12 22:49:28 -0600

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0166fe664262f664c"
   instance_type = "t3.micro"
+  subnet_id = "subnet-0ff39c15a356eeed7"
 
   tags = {
     Name = "HelloWorld"
@@ -24,6 +25,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "web2" {
   ami           = "ami-0166fe664262f664c"
   instance_type = "t2.micro"
+  subnet_id = "subnet-073e8ffbbdb63e3d5"
 
   tags = {
     Name = "jens-deployed."
